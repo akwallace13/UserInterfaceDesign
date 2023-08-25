8a1892e26ca164becc967d4cd025020d29125fc2
akwallace13 <132770092+akwallace13@users.noreply.github.com>
2023-08-25 15:46:40 -0500

    modified:   .DS_Store 	modified:   FinalProjectCSS.css 	modified:   FinalProjectCurrentlyReadingHTML 	deleted:    FinalProjectCurrentlyReadingHTML copy 	deleted:    FinalProjectCurrentlyReadingHTML copy.zip 	modified:   FinalProjectHomePageHTML 	modified:   FinalProjectJS.js 	modified:   FinalProjectJsonData 	new file:   Handlebars.js 	new file:   JQuery.js 	new file:   mustache.js

--- FinalProjectJS.js
+++ FinalProjectJS.js
@@ -1,7 +1,26 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'FinalProjectJsonData');
+ourRequest.onload = function() {
+    if(ourRequest.status >= 200 && ourRequest.status < 400) {
+        var data = JSON.parse(ourRequest.responseText);
+        createHTML(data);
+        console.log(data);
+    } else {
+        console.log("We connected to the server, but it returned an error.");
+    }
+};
 
-app.listen(3000,function()){
-    console.log("Server has been started")
+ourRequest.onerror = function() {
+    console.log("Connection error");
+};
+
+ourRequest.send();
+
+function createHTML(booksData) {
+    var rawTemplate = document.getElementById("booksTemplate").innerHTML;
+    var compiledTemplate = Handlebars.compile(rawTemplate);
+    var ourGeneratedHTML = compiledTemplate(booksData);
+
+    var booksContainer = document.getElementById("books-container");
+    booksContainer.innerHTML = ourGeneratedHTML;
 }
