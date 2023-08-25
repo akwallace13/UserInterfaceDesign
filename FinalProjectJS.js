1b9b2f35b34ab89266b2bec93864f545849e7766
akwallace13 <132770092+akwallace13@users.noreply.github.com>
2023-08-25 10:20:43 -0500

    modified:   FinalProjectCSS.css 	modified:   FinalProjectCurrentlyReadingHTML 	modified:   FinalProjectHomePageHTML 	modified:   FinalProjectJS.js 	new file:   FinalProjectLoanedHTML 	new file:   FinalProjectReadHTML 	new file:   FinalProjectTBRHTML 	modified:   package.json

--- FinalProjectJS.js
+++ FinalProjectJS.js
@@ -1,25 +1,7 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'FinalProjectJsonData');
-ourRequest.onload = function() {
-    if(ourRequest.status >= 200 && ourRequest.status < 400) {
-        var data = JSON.parse(ourRequest.responseText);
-        createHTML(data);
-    } else {
-        console.log("We connected to the server, but it returned an error.");
-    }
-};
+const express = require('express')
+const app = express()
+const PORT = 3000
 
-ourRequest.onerror = function() {
-    console.log("Connection error");
-};
-
-ourRequest.send();
-
-function createHTML(reading) {
-    var rawTemplate = document.getElementById("reading").innerHTML;
-    var compiledTemplate = Handlebars.compile(rawTemplate);
-    var ourGeneratedHTML = compiledTemplate(reading);
-
-    var petsContainer = document.getElementById("read-container");
-    petsContainer.innerHTML = ourGeneratedHTML;
-    }
+app.listen(3000,function()){
+    console.log("Server has been started")
+}
